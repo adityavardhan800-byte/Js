e5baafa9ad92b2af6dc9df18547bcf99a3298ae0
adityavardhan800-byte <adityavardhan800@gmail.com>
2026-05-18 21:07:00 +0000

    constant,var,let learned

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "adityavardhan890@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2 // not allowed
+//console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+this can be used when want to give a multi statement comment 
+*/
