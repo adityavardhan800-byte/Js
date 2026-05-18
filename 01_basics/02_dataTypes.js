70d5f0acf19f997ca5414e0b87573af362220409
adityavardhan800-byte <adityavardhan800@gmail.com>
2026-05-18 21:18:29 +0000

    lct 5 till 8.30 min

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,5 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3) // we are using nodejs, not browser
+
+//code must be readable and future proof
+//try not the make code congested try to make it readible and accessable
